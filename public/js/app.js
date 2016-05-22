505fcd2ca4d4f8098ff44b853e446b0c83f8775b
Jay kelly <Jay kelly>
2016-05-22 05:03:11 +0100

    single page app routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,31 @@
 var app = angular.module('app', ['ngRoute']);
 
+// Routes
+app.config(function($routeProvider) {
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'home.html',
+        controller: 'homeController'
+    })
+    .when('/about', {
+        templateUrl: 'about.html',
+        controller: 'aboutController'
+    })
+    .when('/projects', {
+        templateUrl: 'projects.html',
+        controller: 'projectsController'
+    })
+})
+
+app.controller('homeController', ['$scope', function($scope) {
+    
+}]);
+
+app.controller('aboutController', ['$scope', function($scope) {
+    
+}]);
+
+app.controller('projectsController', ['$scope', function($scope) {
+    
+}]);
