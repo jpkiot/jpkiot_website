036025679dab60853a38609d187f0e72b50d2f3d
Jay Kelly <s3397870@student.rmit.edu.au>
2016-05-27 21:42:41 +1000

    add view engin

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
-app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
         res.render('/public/views/index');
     });
