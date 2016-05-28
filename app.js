af8d3e763dccea4f89b6b751d8c884564b401d69
Ubuntu <ubuntu@ip-172-31-15-50.ap-southeast-2.compute.internal>
2016-05-28 01:25:08 +0000

    change view location - update bower dependencies

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-        res.render('/public/views/index');
+        res.render('index');
     });
 
 app.listen(port, function() {
